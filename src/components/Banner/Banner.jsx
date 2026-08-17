65232bd9c897b0f353dd2770b90e89625a433ac2
Anik <anik.khondokar09@gmail.com>
2026-08-18 01:23:25 +0600

    added BOOKS section with json

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import bannerBook from "../../assets/bannerBook.png";
 
 const Banner = () => {
     return (
-         <div className="max-w-312.5 mx-auto mt-4 flex justify-around items-center bg-[rgba(243, 243, 243, 1)] ">
+         <div className="max-w-312.5 mx-auto flex justify-around py-14 items-center bg-pink-100 p-9 rounded-2xl mt-15">
               <div className="w-1/2">
                 <p className="text-6xl">Books to freshen Up your bookselfs</p>
                 <button className="btn btn-success mt-7 text-2xl p-8">
